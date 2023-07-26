948c523d2532821cc01eb22b111f771c63c919d8
Thom Poppins <thompoppins@gmail.com>
2023-07-26 07:02:27 +0100

    add swipe tindercards functionality with sampledata and set up chat functionality in the frontend

---
+++ src/components/Chat.js
@@ -0,0 +1,5 @@
+const Chat = () => {
+  return <div className="chat-display"></div>;
+};
+
+export default Chat;
---
+++ src/components/ChatHeader.js
@@ -0,0 +1,17 @@
+const ChatHeader = () => {
+  return (
+    <div className="chat-container-header">
+      <div className="profile">
+        <div className="img-container">
+          <img src="" />
+        </div>
+
+        <h3>Username</h3>
+      </div>
+
+      <i className="log-out-icon">Log out</i>
+    </div>
+  );
+};
+
+export default ChatHeader;
