3ce99c091a63fa728f173115019b7cdce2ac08e4
Thom Poppins <thompoppins@gmail.com>
2023-07-26 05:02:04 +0100

    install react-tinder-card and @react-spring/web 9.4.5

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,68 @@
+import TinderCard from "react-tinder-card";
+import { useState } from "react";
+
+// TODO: write documentation
 const Dasboard = () => {
-  return <div>Dashboard</div>;
+  // fill const characters with sampledata
+  const characters = [
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/wUgn48u.jpeg",
+    },
+    {
+      name: "Erlich Bachman",
+      url: "https://i.imgur.com/wUgn48u.jpeg",
+    },
+    {
+      name: "Monica Hall",
+      url: "https://i.imgur.com/wUgn48u.jpeg",
+    },
+    {
+      name: "Jared Dunn",
+      url: "https://i.imgur.com/wUgn48u.jpeg",
+    },
+    {
+      name: "Dinesh Chugtai",
+      url: "https://i.imgur.com/wUgn48u.jpeg",
+    },
+  ];
+
+  // save last direction
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
+
+  return (
+    <div className="dashboard">
+      {/* <ChatContainer /> */}
+      <div className="swiper-container">
+        <div className="card-container">
+          {characters.map((character) => (
+            <TinderCard
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+            >
+              <div
+                style={{ backgroundImage: "url(" + character.url + ")" }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dasboard;
